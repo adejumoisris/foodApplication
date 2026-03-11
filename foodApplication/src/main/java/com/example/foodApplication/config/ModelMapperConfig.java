1534c84d416438c4116e55becb175cbe1a1f8ebd
idris adejumo <idris.adejumo@novabank.ng>
2026-03-11 11:06:15 +0100

    model Mapper

---
+++ foodApplication/src/main/java/com/example/foodApplication/config/ModelMapperConfig.java
@@ -0,0 +1,19 @@
+package com.example.foodApplication.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+        return modelMapper;
+    }
+}
