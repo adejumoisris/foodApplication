6ed09eb51c13fa8c0766a83638b422de1515c0ca
idris adejumo <idris.adejumo@novabank.ng>
2026-03-11 11:54:49 +0100

    Exceptional Handling

---
+++ foodApplication/src/main/java/com/example/foodApplication/exception/PaymentProcessingException.java
@@ -0,0 +1,7 @@
+package com.example.foodApplication.exception;
+
+public class PaymentProcessingException extends RuntimeException{
+    public PaymentProcessingException(String message) {
+        super(message);
+    }
+}
